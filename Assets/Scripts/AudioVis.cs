ec39d93f5641e5fb5e277efe75d653f799dd22ca
Ian Brock <ibpositive@live.com>
2020-05-06 10:03:27 -0400

    Init Commit - A2
    Adding basic menus, camera movement, and foundation for scene animations.

---
+++ Assets/Scripts/AudioVis.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+public class AudioVis : MonoBehaviour
+{
+    AudioSource audioSource;
+    public static float[] samples = new float[512];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        //Debug.Log(samples[3]);
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+}
---
+++ Assets/Scripts/InstantiateCubes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class InstantiateCubes : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    GameObject[] cube = new GameObject[20];
+    public float maxScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject instanceCube = (GameObject)Instantiate(cubePrefab);
+            instanceCube.transform.position = this.transform.position;
+            instanceCube.transform.parent = this.transform;
+            instanceCube.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, 18f * i, 0);
+            instanceCube.transform.position = Vector3.forward * 100;
+            cube[i] = instanceCube;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < 20; i++)
+        {
+            if(cube != null)
+            {
+                //https://youtu.be/Ri1uNPNlaVs?t=208
+                cube[i].transform.localScale = new Vector3(10, AudioVis.samples[i] * maxScale +2, 10);
+            }
+        }
+    }
+}
